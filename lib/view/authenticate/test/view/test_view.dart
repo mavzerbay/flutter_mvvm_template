9520ae90e4e0e48af9d92eb65680bea66c4b10ba
mavzerbay <mavzerbay@gmail.com>
2021-03-21 16:41:10 +0300

    Application Language & Theme

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,8 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:fluttermvvmtemplate/core/base/state/base_state.dart';
-import 'package:fluttermvvmtemplate/core/base/view/base_view.dart';
-import 'package:fluttermvvmtemplate/view/authenticate/test/viewmodel/test_view_model.dart';
+
+import '../../../../core/base/state/base_state.dart';
+import '../../../../core/base/view/base_view.dart';
+import '../../../../core/extensions/string_extension.dart';
+import '../../../../core/init/lang/language_manager.dart';
+import '../../../../core/init/lang/locale_keys.g.dart';
+import '../viewmodel/test_view_model.dart';
 
 class TestsView extends StatefulWidget {
   @override
@@ -23,9 +28,24 @@ class _TestsViewState extends BaseState<TestsView> {
   }
 
   Widget get scaffoldBody => Scaffold(
+        appBar: AppBar(
+          title: Text(LocaleKeys.welcome.locale),
+          actions: [
+            iconButtonChangeTheme(),
+          ],
+        ),
         floatingActionButton: floatingActionButtonNumberIncrement,
         body: textNumber,
       );
+
+  IconButton iconButtonChangeTheme() {
+    return IconButton(
+      icon: Icon(Icons.change_history),
+      onPressed: () async {
+        await context.setLocale(LanguageManager.instance.enLocale);
+      },
+    );
+  }
 
   FloatingActionButton get floatingActionButtonNumberIncrement {
     return FloatingActionButton(
--- lib/view/authenticate/test/viewmodel/test_view_model.dart
+++ lib/view/authenticate/test/viewmodel/test_view_model.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/constants/enums/app_theme_enum.dart';
+import '../../../../core/init/notifier/theme_notifier.dart';
+
 part 'test_view_model.g.dart';
 
 class TestViewModel = _TestViewModelBase with _$TestViewModel;
 
 abstract class _TestViewModelBase with Store {
+  BuildContext context;
+
+  void setContext(BuildContext context) {
+    this.context = context;
+  }
+
   @observable
   int number = 0;
 
@@ -13,5 +25,9 @@ abstract class _TestViewModelBase with Store {
   @action
   void incrementNumber() {
     number++;
+  }
+
+  void changeTheme() {
+    Provider.of<ThemeNotifier>(context, listen: false).changeValue(AppThemes.DARK);
   }
 }
