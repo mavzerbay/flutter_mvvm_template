4dc67b5c334dc104f8b3263902445df823dffd49
mavzerbay <mavzerbay@gmail.com>
2021-03-21 15:43:40 +0300

    State Management (Mobx & MVVM)

---
+++ lib/view/authenticate/test/view/test_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:fluttermvvmtemplate/core/base/state/base_state.dart';
+import 'package:fluttermvvmtemplate/core/base/view/base_view.dart';
+import 'package:fluttermvvmtemplate/view/authenticate/test/viewmodel/test_view_model.dart';
+
+class TestsView extends StatefulWidget {
+  @override
+  _TestsViewState createState() => _TestsViewState();
+}
+
+class _TestsViewState extends BaseState<TestsView> {
+  TestViewModel viewModel;
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<TestViewModel>(
+      viewModel: TestViewModel(),
+      onModelReady: (model) {
+        viewModel = model;
+      },
+      onPageBuilder: (context, value) => scaffoldBody,
+    );
+  }
+
+  Widget get scaffoldBody => Scaffold(
+        floatingActionButton: floatingActionButtonNumberIncrement,
+        body: textNumber,
+      );
+
+  FloatingActionButton get floatingActionButtonNumberIncrement {
+    return FloatingActionButton(
+      onPressed: () => viewModel.incrementNumber(),
+    );
+  }
+
+  Widget get textNumber {
+    return Observer(
+      builder: (context) => Text(
+        viewModel.number.toString(),
+      ),
+    );
+  }
+}
